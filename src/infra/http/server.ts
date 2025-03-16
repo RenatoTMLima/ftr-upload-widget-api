66e28b1a7c552ffe00b03489c8903d70c27f082b
Renato T M Lima <renato.matura@gmail.com>
2025-03-15 23:52:54 -0400

    Upload report csv and github actions

--- src/infra/http/server.ts
+++ src/infra/http/server.ts
@@ -8,6 +8,7 @@ import {
   serializerCompiler,
   validatorCompiler,
 } from 'fastify-type-provider-zod'
+import { exportUploadsRoute } from './routes/export-uploads'
 import { getUploadsRoute } from './routes/get-uploads'
 import { uploadImageRoute } from './routes/upload-image'
 import { transformSwaggerSchema } from './transform-swagger-schema'
@@ -47,6 +48,7 @@ server.setErrorHandler((error, request, reply) => {
 
 server.register(uploadImageRoute)
 server.register(getUploadsRoute)
+server.register(exportUploadsRoute)
 
 server.listen({ port: 3333, host: '0.0.0.0' }).then(() => {
   console.log('HTTP server running!')
